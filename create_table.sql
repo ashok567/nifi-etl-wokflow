c568f2361210ceea539c175cf44d60d8b0561890
Ashok Thakur <iashok180@gmail.com>
2021-03-11 13:28:28 +0530

    MODIFY: nifi flow updated

--- create_table.sql
+++ create_table.sql
@@ -1,2 +1,2 @@
-CREATE TABLE IF NOT EXISTS demo.districts (recorded_date string, state string, district string, confirmed int, recovered int, deceased int, other int, tested int)
+CREATE TABLE IF NOT EXISTS demo.districts (recorded_date string, state string, district string, confirmed string, recovered string, deceased string, other string, tested string)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',';
